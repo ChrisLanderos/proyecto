7448d93d756a692ec4a950e8aa88ca4010f48fb3
Sopatun <zopatulin@gmail.com>
2025-12-04 00:50:20 -0600

    Transicion de un estado a otro

--- Transicion.java
+++ Transicion.java
@@ -1,4 +1,16 @@
-
 public class Transicion {
+    Estado desde;
+    char simbolo;
+    Estado hacia;
 
+    public Transicion(Estado desde, char simbolo, Estado hacia) {
+        this.desde = desde;
+        this.simbolo = simbolo;
+        this.hacia = hacia;
+    }
+
+    @Override
+    public String toString() {
+        return desde + " --" + simbolo + "--> " + hacia;
+    }
 }
